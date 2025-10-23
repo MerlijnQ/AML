b5a970e7cd6c93e0f5ccccc6c1057a85c39c5c37
MartijnSchippers <martijn.schippers2@gmail.com>
2025-10-23 11:03:05 +0200

    The heatmap is visualized

--- figures/visualize_heatmap.py
+++ figures/visualize_heatmap.py
@@ -1,5 +1,6 @@
 import json
 import matplotlib.pyplot as plt
+import os
 import seaborn as sns
 
 with open('model_data.json', 'r') as f:
@@ -12,49 +13,48 @@ shap_values = results["shap_values"]
 
 nr_features = len(shap_values[0])
 
+def create_heatmap(shap_result, subtitle=""):
+    # retrieve the data to form a heatmap. 
+    # save the data structure in such a way that sns.heatmap knows what to do with it
+    def get_heatmap_data(shap_result):
+        global nr_features
+        # global 
+        heatmap_data = [[0, 0] for i in range(nr_features)]
+        nr_features = len(shap_result)
 
-data = [
-    [["nat_demand", 0.002346440078836167],
-     ["T2M_toc", 0.013409032346535243]],
-    [["T2M_toc", 0.039039051532745374],
-     [None, None]]
-]
+        # add the rows to the data column
+        # a row here is all values corresponding to all features to a certain run
+        for run_nr, pairs in enumerate(shap_result):
+            number_row = [pair[1] for pair in pairs]
+            # insert a zero to the row in front until full
+            for i in range(run_nr):
+                number_row = [float('NaN')] + number_row
 
-# change data structure in such a way that sns.heatmap knows what to do with it
-shap_result = results["shap_values"][0]
-nr_features = len(shap_result)
-shap_data = [[0, 0] for i in range(nr_features)]
+            heatmap_data[run_nr] = number_row
 
-for run_nr, pairs in enumerate(shap_result):
-    # add a zero pair to the pair until full
-    number_row = [kaas[1] for kaas in pairs]
-    for i in range(run_nr):
-        # pairs = [[['kaas', 0]] + pairs]
-        number_row = [0] + number_row
-    shap_data[run_nr] = number_row
+        # return the inverse, so each row has values of all runs of the same feature
+        return [list(row) for row in zip(*heatmap_data)]
 
-# now inverse the matrix
-shap_data = [list(row) for row in zip(*shap_data)]
-print(shap_data)
+    def plot_heatmap():
+        labels = [label[0] for label in shap_values[0][0]]
+        # annot_labels = [[("" if val == 0 else val) for val in row] for row in heatmap_data]
+        annot_labels = [
+            ["" if val == float('NaN') else round(val, 4) for val in row]
+            for row in heatmap_data
+        ]
+        print(annot_labels)
+        # sns.heatmap(heatmap_data, annot=True, cmap="coolwarm", yticklabels=labels)
+        sns.heatmap(heatmap_data, annot=annot_labels, fmt="", cmap="coolwarm", cbar=False, yticklabels=labels)
+        plt.xticks(ticks=[x + 0.5 for x in range(nr_features)], labels=list(range(nr_features, 0, -1)))
+        plt.xlabel("nr_features")
+        plt.ylabel("features")
+        save_directory = 'heatmap'
+        os.makedirs(save_directory, exist_ok=True)
+        plt.savefig(save_directory + "/heatmap" + subtitle + ".pdf")
+        plt.show()
+    heatmap_data = get_heatmap_data(shap_result)
+    plot_heatmap()
 
-labels = [label[0] for label in shap_result[0]]
-
-
-sns.heatmap(shap_data, annot=True, cmap="coolwarm", yticklabels=labels)
-plt.xlabel("nr_features")
-plt.ylabel("features")
-plt.show()
-# input: [window_24, window_48, window_72]: 3
-#           where window = [feature_values_1, feature_values_2, ..]: nr_features
-#           where feature_values = [(feature_name, shap_value), (), ..]: nr_features->1
-# nr_features = 20
-
-# def create_window():
-#     return []
-
-# def create_random_input():
-#     windows = [create_window for i in range()]
-
-#     return windows
-
-# randomized_input = create_random_input()
+create_heatmap(results["shap_values"][0], "T0")
+create_heatmap(results["shap_values"][1], "T1")
+create_heatmap(results["shap_values"][2], "T2")
