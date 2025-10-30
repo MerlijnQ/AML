4552ef7b19d25ed0f312e0d5418ce930fb36cc6f
luca <lucacolli5@gmail.com>
2025-10-30 16:21:03 +0100

    Created class for SHAP and removed some test comments

--- pipeline.py
+++ pipeline.py
@@ -1,41 +1,30 @@
 from dataloader import DataLoaderTimeSeries
 from model.ensemble import create_ensemble
-from shap_explainer import explain_predictions
-from datetime import datetime
+from shap_explainer import SHAPExplainer
 import json
 import sys
 
 if __name__ == "__main__":
     model_data = {
-        "epistemic_uncertainty": [[],[],[]],
-        "aleatoric_uncertainty": [[],[],[]],
-        "accuracy": [[],[],[]],
-        "discarded": [[],[],[]],
-        "shap_values": [[],[],[]]
+        "epistemic_uncertainty": [[]],
+        "aleatoric_uncertainty": [[]],
+        "accuracy": [[]],
+        "discarded": [[]],
+        "shap_values": [[]]
     }
-
-    #window_sizes = [24, 48, 72, 120]
 
     args = sys.argv[1:]
     window_sizes = [int(args[0])]
 
-    # timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    # file_name = f"model_data_{timestamp}.json"
+    explainer = SHAPExplainer()
 
     for s in range(len(window_sizes)):
         print(f"Window size: {window_sizes[s]}")
 
         data_loader = DataLoaderTimeSeries(window_sizes[s])
 
-        num_features = len(data_loader.features)
+        n = len(data_loader.features)
 
-        # For testing
-        # for n in range(0, num_features-5):
-        #     data_loader.remove_feature(data_loader.get_feature_at_index(0))
-        # num_features = len(data_loader.features)
-        # print(data_loader.features)
-
-        n = num_features
         while n > 0:
             print(f"Number of features: {n}")
 
@@ -49,8 +38,7 @@ if __name__ == "__main__":
             model = ensemble_builder.get_ensemble_model() # Model returns rmse, epistemic, aleatoric on prediction
 
             # Evaluate model
-            # rmse, epi, alea = model.evaluate(data_loader.test_loader)
-            rmse, epi, alea = model.evaluate(data_loader) #pass entire object to rescale for rmse calculation
+            rmse, epi, alea = model.evaluate(data_loader) # pass entire object to rescale for rmse calculation
             print(f"RMSE: {rmse}, epistemic: {epi}, aleatoric: {alea}")
 
             # Store uncertainties and accuracy
@@ -59,7 +47,7 @@ if __name__ == "__main__":
             model_data["accuracy"][s].append(rmse)
 
             # Feature selection with SHAP
-            current_shap_values, discarded_feature = explain_predictions(
+            current_shap_values, discarded_feature = explainer.explain_predictions(
                 X_train=data_loader.train_loader,
                 X_test=data_loader.test_loader,
                 model=model,
